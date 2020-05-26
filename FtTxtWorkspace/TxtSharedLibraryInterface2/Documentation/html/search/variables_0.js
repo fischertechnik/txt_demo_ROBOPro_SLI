3fd5947d09efdbbcef81ec86b185c25429334620
Carel van Leeuwen <C.vanLeeuwen@alumnus.utwente.nl>
2020-05-26 08:59:47 +0200

    update

---
+++ FtTxtWorkspace/TxtSharedLibraryInterface2/Documentation/html/search/variables_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['isinit_21',['IsInit',['../_lib_example_s_l_i2_8cpp.html#a5b09477f0320cb15a0f387190f0aa26d',1,'LibExampleSLI2.cpp']]],
+  ['isstop_22',['IsStop',['../_lib_example_s_l_i2_8cpp.html#a7ad5adbc3a742b9ea60ce78b44f31cb9',1,'LibExampleSLI2.cpp']]]
+];
